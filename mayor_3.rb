e24be6f1e629735e4bd2b5eed2608e1e4c4b38e2
alicia0408 <alicent.08.02@gmail.com>
2021-05-18 19:24:16 -0400

    correcciones del codigo mayor a 3

--- mayor_3.rb
+++ mayor_3.rb
@@ -3,9 +3,9 @@ valor_2 = ARGV[1].to_i
 valor_3 = ARGV[2].to_i
 
 if valor_1 > valor_2 && valor_1 > valor_3
-  puts valor_1
+  puts "El valor #{ valor_1} es mayor"
 elsif valor_2 > valor_3
-  puts valor_2
+  puts "El valor #{ valor_2} es mayor"
 else
-  puts valor_3
+  puts "El valor #{ valor_3} es mayor"
 end
